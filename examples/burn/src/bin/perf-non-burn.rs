cdc03ae541532a95ca6aebcd941da05beca026a9
Bastian Bloessl <mail@bastibl.net>
2025-07-08 13:10:01 +0200

    ads

---
+++ examples/burn/src/bin/perf-non-burn.rs
@@ -0,0 +1,79 @@
+use anyhow::Result;
+use futuresdr::blocks::Fft;
+use futuresdr::blocks::FftDirection;
+use futuresdr::blocks::FileSource;
+use futuresdr::blocks::NullSink;
+use futuresdr::prelude::*;
+use futuresdr_burn::BATCH_SIZE;
+use futuresdr_burn::FFT_SIZE;
+
+#[derive(Block)]
+struct Avg {
+    #[input]
+    input: circular::Reader<Complex32>,
+    #[output]
+    output: circular::Writer<f32>,
+}
+
+impl Avg {
+    fn new() -> Self {
+        Self {
+            input: Default::default(),
+            output: Default::default(),
+        }
+    }
+}
+
+impl Kernel for Avg {
+    async fn work(
+        &mut self,
+        io: &mut WorkIo,
+        _m: &mut MessageOutputs,
+        _b: &mut BlockMeta,
+    ) -> Result<()> {
+        let input = self.input.slice();
+        let output = self.output.slice();
+
+        if input.len() >= FFT_SIZE * BATCH_SIZE && output.len() >= FFT_SIZE {
+
+            for i in 0..FFT_SIZE {
+                let mut sum = 0.0;
+                for b in 0..BATCH_SIZE {
+                    sum += input[b * FFT_SIZE + i].norm_sqr();
+                }
+                output[i] = sum / BATCH_SIZE as f32;
+            }
+
+            self.input.consume(FFT_SIZE * BATCH_SIZE);
+            self.output.produce(FFT_SIZE);
+        }
+
+        if self.input.finished() {
+            let input = self.input.slice();
+            if input.len() < FFT_SIZE * BATCH_SIZE {
+                io.finished = true;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    futuresdr::runtime::init();
+    let mut fg = Flowgraph::new();
+
+    let src = FileSource::<Complex32>::new("data.cf32", false);
+    let fft = Fft::with_options(FFT_SIZE, FftDirection::Forward, true, None);
+    let avg = Avg::new();
+    let snk = NullSink::<f32>::new();
+
+    connect!(fg, src > fft > avg > snk);
+
+    let now = std::time::Instant::now();
+    Runtime::with_scheduler(futuresdr::runtime::scheduler::SmolScheduler::new(1, true)).run(fg)?;
+    let elapsed = now.elapsed();
+    println!("took {elapsed:?}");
+
+    Ok(())
+}
