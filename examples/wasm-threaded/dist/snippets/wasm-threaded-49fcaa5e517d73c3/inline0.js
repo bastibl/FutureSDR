6b3c7f73530697b4afd5f54ed91504d5e1cf4b1a
Bastian Bloessl <mail@bastibl.net>
2026-05-09 12:12:15 +0200

    asdf

--- examples/wasm-threaded/dist/snippets/wasm-threaded-49fcaa5e517d73c3/inline0.js
+++
@@ -1,6 +0,0 @@
-
-export function futuresdr_is_scheduler_worker() {
-  const isWorker = globalThis.__futuresdr_scheduler_worker === true;
-  console.info("FutureSDR Rust main", isWorker ? "scheduler-worker" : "browser-main");
-  return isWorker;
-}
